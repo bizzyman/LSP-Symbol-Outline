20f025c7d525ca870ccb43431d1a8ddb6b691cd5
bizzyman <bezmanalbert@yahoo.co.uk>
2018-01-22 00:02:00 +0000

    add package header

--- lsp-symbol-outline.el
+++ lsp-symbol-outline.el
@@ -1,4 +1,41 @@
-;;; LSP SYMBOL OUTLINE
+;;; lsp-symbol-outline.el --- a symbol tree for lsp-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 bizzyman
+
+;; Version: 0.0.1
+;; Homepage: https://github.com/bizzyman/LSP-Symbol-Outline
+;; Keywords: languages, lsp, outline
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and-or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;; Commentary:
+
+;;
+;; Call (lsp-symbol-outline-create-buffer-window) to enable.
+;;
+;;  TODO:
+;;
+;;  - Refactor
+;;  - Introduce configuration
+;;
+
+;;; Code:
 
 (require 'lsp-mode)
 (require 'ov)
